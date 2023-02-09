012846ee912b01ad6efb9ba8b396b2b7e743228b
Maks <crossstar66@gmail.com>
2023-02-09 12:42:36 +0500

    Дописана логика в js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,31 @@ typingInfo = document.getElementById("typingInfo")
 sendButton = document.getElementById("sendButton")
 userNameInput = document.getElementById("userName")
 messageInput = document.getElementById("text")
-const url = ("ws://127.0.0.1:8080/game/"+name)
+const game = document.querySelector(".game")
+
+
+let playerList = new Map()
+
+const player = document.createElement('div')
+player.className = "player"
+player.style.width = "100px"
+player.style.height = "100px"
+player.style.backgroundColor = "#FF0010"
+
+game.append(player)
+playerList.set(playerName,player)
+
+const url = ("ws://127.0.0.1:8080/game/"+ playerName)
 let ws = new WebSocket (url)
-let typingUsers = new Map()
+let typingUser = new Map()
+
 let keyPressed = {
     ArrowLeft: false,
     ArrowRight: false,
     ArrowUp: false,
     ArrowDown: false,
 }
+
 document.onkeydown = (e) => {
     keyPressed[e.code] = true
 }
@@ -19,65 +35,6 @@ document.onkeyup = (e) => {
     keyPressed[e.code] = false
 }
 
-ws.onmessage = e =>{
-    const data = JSON.parse(e.data)
-    console.log(e.data);
-    switch (data.type){
-        case 1:
-            createMessage(data)
-            break;
-        case 2:
-            typingUsers.set(data.name, 0)
-
-            updateTyping()
-
-            setTimeout(()=>{
-                typingUsers.clear()
-                typingInfo.textContent = ""
-            }, 3000)
-            break
-    }
-}
-ws.onclose = e =>{
-    setTimeout(() =>{
-        ws = new WebSocket(url)
-    }, 1000)
-}
-// sendButton.onclick = () =>{
-//     ws.send(JSON.stringify({
-//         type: 1,
-//         name: userNameInput.value,
-//     }))
-// }
-sendButton.onclick = () => {
-    ws.send(JSON.stringify({
-        type: 1,
-        data: JSON.stringify({
-            name: playerName,
-            message: messageInput.value,
-        })
-
-    }))
-}
-messageInput.oninput = () =>{
-    if (messageInput. value.length > 0){
-        ws.send(JSON.stringify({
-            type: 2,
-            data: JSON.stringify({
-                name: playerName,
-            })
-        }))
-    }
-}
-
-let client = {
-    Name: "asd",
-    Message: "HELLO, Im  MAKSIM"
-
-}
-function createMessage(data) {
-
-}
 function SendCoords(){
     let direction = {
         x: 0,
@@ -97,29 +54,118 @@ function SendCoords(){
             direction.y = 1
         }
     }
-        ws.send(JSON.stringify({
-            type: 3,
-            data: JSON.stringify(direction)
-        }))
+    ws.send(JSON.stringify({
+        type: 3,
+        data: JSON.stringify(direction)
+    }))
 }
+
+ws.onmessage = e =>{
+    const data = JSON.parse(e.data)
+    console.log(e.data);
+
+    const dataMesseng = JSON.parse(data.data)
+
+    switch (data.type){
+        case 1:
+            createMessage(dataMesseng)
+            break;
+        case 2:
+            playerList.set(dataMesseng.name, 0)
+
+            updateTyping()
+
+            setTimeout(()=>{
+                playerList.clear()
+                typingInfo.textContent = ""
+            }, 3000)
+            break
+        case 3:
+            UpdateFrame(dataMesseng)
+            break
+    }
+}
+ws.onclose = e =>{
+    setTimeout(() =>{
+        ws = new WebSocket(url)
+    }, 1000)
+}
+
+sendButton.onclick = () =>{
+    ws.send(JSON.stringify({
+        type: 1,
+        data:JSON.stringify({
+            name:playerName
+        }) 
+    }))
+}
+
+sendButton.onclick = () => {
+    ws.send(JSON.stringify({
+        type: 1,
+        data: JSON.stringify({
+            name: playerName,
+            message: messageInput.value,
+        })
+
+    }))
+}
+
+messageInput.oninput = () =>{
+    if (messageInput. value.length > 0){
+        ws.send(JSON.stringify({
+            type: 2,
+            data: JSON.stringify({
+                name: playerName,
+            })
+        }))
+    }
+}
+function createMessage(data){
+    const messegeDiv = document.createElement("div")
+    const userName = document.createElement("span")
+    const userMsg = document.createElement("span")
+
+    messegeDiv.append(userName,userMsg)
+    userName.textContent = data.name +": "
+    userMsg.textContent = data.message
+    messegContainer.append(messegeDiv)
+}
+
+// let client = {
+//     Name: "asd",
+//     Message: "HELLO, Im  MAKSIM"
+
+// }
+
+
+
 setInterval(SendCoords, 25)
+
 function updateTyping() {
     let typing = []
-    for (let [k,v] of typingUsers){
+    for (let k of typingUser){
         console.log(k)
         typing.push(k)
     }
-    console.log(typingUsers)
-        if (typingUsers.size > 1){
+    console.log(playerList)
+        if (playerList.size > 1){
             typingInfo.textContent = typing.join(", ") +  " набирают сообщение..."
         }
         else{
             typingInfo.textContent = typing.join(", ") +  " набирает сообщение..."
         }
     }
+
 function UpdateFrame (players){
     for (let player of players) {
         const p = playerList.get(player.name)
+        if(player.isDead == true){
+            if(p) p.remove()
+            playerList.delete(player.name)
+            continue
+        
+        }
         if (p) {
             p.style.left = player.coords.X + "px"
             p.style.top = player.coords.Y + "px"
@@ -131,14 +177,15 @@ function UpdateFrame (players){
             p1.style.width = "100px"
             p1.style.height = "100px"
             p1.style.backgroundColor = "#000000"
+            p1.textContent = "Саблезубый ййй"
 
             game.append(p1)
             playerList.set(player.name, p1)
         }
     }
 }
+
     const A = {
         x: 0,
         y: 0,
     }
-    HashChangeEvent(OfflineAudioCompletionEvent).FRAMEBUFFER_ATTACHMENT_OBJECT_NAME(KeyboardEvent).name
